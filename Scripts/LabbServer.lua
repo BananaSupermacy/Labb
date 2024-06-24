a8bcf5c56b7ee9c8a6350b7f3322aef9c4bc831a
MrDevBanana <141438800+BananaSupermacy@users.noreply.github.com>
2024-06-25 00:23:09 +0200

    Labb

--- Scripts/LabbServer.lua
+++ Scripts/LabbServer.lua
@@ -1,26 +1,26 @@
-local Loader = {}
-Loader.__index = Loader
-Loader.Storage = {}
-Loader.State = "Loading"
+local Labb = {}
+Labb.__index = Labb
+Labb.Storage = {}
+Labb.State = false
 
 --local CrossSignals = require(game.ReplicatedStorage.UnpackAtReplicatedStorage.Utils.CrossSignal)
 
-function Loader:Await()
-	if Loader.State ~= "Loaded" then
+function Labb:Await()
+	if not Labb.State then
 		while(wait()) do
-			if Loader.State == "Loaded" then break end
+			if Labb.State then break end
 		end
 	end
 end
 
-function Loader.AddHandler(Handler: {})
-	local meta =  setmetatable(Handler, Loader)
-	table.insert(Loader.Storage, meta)
+function Labb.AddHandler(Handler: {})
+	local meta =  setmetatable(Handler, Labb)
+	table.insert(Labb.Storage, meta)
 	return meta
 end
 
-function Loader.GetHandler(Name: string)
-	for _, v in pairs(Loader.Storage) do
+function Labb.GetHandler(Name: string)
+	for _, v in pairs(Labb.Storage) do
 		if v and v.Name == Name then
 			local succ, err = pcall(function()
 				v:GatherEvent()
@@ -37,10 +37,10 @@ end
 
 
 --error handlers
-setmetatable(Loader, {
+setmetatable(Labb, {
 	__index = function(t, key) warn(("This key doesnt exist, KEY: %s"):format(tostring(key)), "\n-> END REPORT <-") end;
 	--__newindex = function(t, key) warn(("Key to which you are trying asign value doesnt exist(Adding keys blacklisted) KEY: %s"):format(tostring(key)), "\n-> END REPORT <-") end;
 })
 
 
-return Loader
+return Labb
